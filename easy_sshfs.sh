ae650634026818a8b0b0011681d9f36c425b40b4
hyliang96 <hyliang96@hotmail.com>
2024-01-05 16:14:55 +0800

    modified easy_sshfs.sh and remote_open.app/Contents/document.wflow

--- easy_sshfs.sh
+++ easy_sshfs.sh
@@ -6,7 +6,7 @@ this_dir_abs_path=$(cd "$(dirname "${BASH_SOURCE[0]-$0}")"; pwd)
 
 
 # 【自己设置一个文件夹，其下专门用来sshfs挂载远程目录】
-mount_dir="/Users/mac/Desktop/mount"
+mount_dir="/Users/mac/mount"
 # 【本地nc端口】
 port=8304
 
